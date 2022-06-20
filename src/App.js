2a638fc3a353dca4344c0ec9e4393b28cf3682e4
이우상 <usang12@iusang-ui-MacBookAir.local>
2022-06-21 08:39:22 +0900

    usang1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      <div className="box-wrapper">
+        <div className="box-1" />
+        <div className="box-2" />
+        <div className="box-3" />
+      </div>
+  );
+}
+
+export default App;
